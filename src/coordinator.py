f33ba35aab7ec7f8c0c49c0ca15d023dd7956a3b
Petros Ypsilantis <pet_yps@hotmail.com>
2025-12-28 13:03:56 +0000

    add the coordinator to connect with the remote agents

---
+++ src/coordinator.py
@@ -0,0 +1,268 @@
+from src.remote_agent_connection import TaskUpdateCallback, RemoteAgentConnections
+from a2a.types import (
+    AgentCard, SendMessageRequest, SendMessageResponse, SendMessageSuccessResponse, MessageSendParams, Task
+)
+from a2a.client import A2ACardResolver
+from google.adk import Agent
+from google.adk.tools.tool_context import ToolContext
+from google.adk.agents.callback_context import CallbackContext
+from google.adk.agents.readonly_context import ReadonlyContext
+
+from typing import List, Dict
+import httpx
+import uuid
+import os
+import asyncio
+
+
+class CoordinatorAgent:
+    """The Coordinator agent.
+    This is the client agent responsible for sending tasks to agents
+    """
+
+    def __init__(
+            self, task_callback: TaskUpdateCallback | None = None,
+    ):
+        self.task_callback = task_callback
+        self.remote_agent_connections: dict[str, RemoteAgentConnections] = {}
+        self.cards: Dict[str, AgentCard]
+        self.agents: str = ""
+
+    async def _async_init_components(self, remote_agent_addresses: List[str]) -> None:
+
+        """Asynchronous part of initiation"""
+        async with httpx.AsyncClient(timeout=30) as client:
+            for address in remote_agent_addresses:
+                card_resolver = A2ACardResolver(
+                    client, address
+                )  # Constructor is sync
+                try:
+                    card = (
+                        await card_resolver.get_agent_card()
+                    )
+
+                    remote_connection = RemoteAgentConnections(
+                        agent_card=card, agent_url=address
+                    )
+                    self.remote_agent_connections[card.name] = remote_connection
+                    self.cards[card.name] = card
+
+                except httpx.ConnectError as e:
+                    print(
+                        f'ERROR: Failed to get agent card from {address}: {e}'
+                    )
+
+
+                except Exception as e:  # Catch other potential errors
+                    print(
+                        f'ERROR: Failed to initialize connection for {address}: {e}'
+                    )
+
+        # Populate self.agents using the logic from original __init__ (via list_remote_agents)
+        agent_info = []
+        for agent_detail_dict in self.list_remote_agents():
+            agent_info.append(json.dumps(agent_detail_dict))
+        self.agents = '\n'.join(agent_info)
+
+
+    @classmethod
+    async def create(
+            cls,
+            remote_agent_adresses: List[str],
+            task_callback: TaskUpdateCallback | None = None
+    ) -> 'CoordinatorAgent':
+
+        instance = cls(task_callback)
+        await instance._async_init_components(remote_agent_adresses)
+        return instance
+
+    def create_agent(self) -> Agent:
+        """Create an instance of the CoordinatorAgent."""
+        model_id = 'gemini-2.5-flash'
+        print(f'Using hardcoded model: {model_id}')
+        return Agent(
+            model=model_id,
+            name='Product_agent',
+            instruction=self.root_instruction,
+            before_model_callback=self.before_model_callback,
+            description=(
+                'This coordinator agent orchestrates the product information agents'
+            ),
+            tools=[
+                self.send_message,
+            ],
+        )
+
+    def root_instruction(self, context: ReadonlyContext) -> str:
+
+        """Generate root instruction for the CoordinatorAgent"""
+        current_agent = self.check_active_agent(context)
+        return f"""
+        **Role:** You are the central product information coordination agent. Your primary function is to manage the 
+        extraction of product information. Upon receiving a product information request from the user, you will perform 
+        the following tasks and then return the final polished content:
+        
+        Task 1. **Identify the required information**
+        Task 2. **Reply back to the user request in a clear anc concise way**
+
+        **Core Directives:**
+
+        * **Task Delegation:** Utilize the `send_message` function to assign each task to a remote agent.
+        * **Contextual Awareness for Remote Agents:** If a remote agent repeatedly requests user confirmation, assume it lacks access to the full conversation history. In such cases, enrich the task description with all necessary contextual information relevant to that specific agent.
+        * **Autonomous Agent Engagement:** Never seek user permission before engaging with remote agents. If multiple agents are required to fulfill a request, connect with them directly without requesting user preference or confirmation.
+        * **Transparent Communication:** Always present the complete and detailed response from the remote agent to the user.
+        * **User Confirmation Relay:** If a remote agent asks for confirmation, and the user has not already provided it, relay this confirmation request to the user.
+        * **Focused Information Sharing:** Provide remote agents with only relevant contextual information. Avoid extraneous details.
+        * **No Redundant Confirmations:** Do not ask remote agents for confirmation of information or actions.
+        * **Tool Reliance:** Strictly rely on available tools to address user requests. Do not generate responses based on assumptions. If information is insufficient, request clarification from the user.
+        * **Prioritize Recent Interaction:** Focus primarily on the most recent parts of the conversation when processing requests.
+        * **Active Agent Prioritization:** If an active agent is already engaged, route subsequent related requests to that agent using the appropriate task update tool.
+
+        **Agent Roster:**
+
+        * Available Agents: `{self.agents}`
+        * Currently Active Agent: `{current_agent['active_agent']}`
+        
+        """
+
+
+    def check_active_agent(self, context: ReadonlyContext):
+        state = context.state
+        if (
+                'session_id' in state
+                and 'session_active' in state
+                and state['session_active']
+                and 'active_agent' in state
+        ):
+            return {'active_agent': f'{state["active_agent"]}'}
+        return {'active_agent': 'None'}
+
+
+    def before_model_callback(
+            self, callback_context: CallbackContext, llm_request
+    ):
+        state = callback_context.state
+        if 'session_active' not in state or not state['session_active']:
+            if 'session_id' not in state:
+                state['session_id'] = str(uuid.uuid4())
+            state['session_active'] = True
+
+    def list_remote_agents(self):
+        """List the available remote agents you can use to delegate the task."""
+        if not self.cards:
+            return []
+
+        remote_agent_info = []
+        for card in self.cards.values():
+            print(f'Found agent card: {card.model_dump(exclude_none=True)}')
+            print('=' * 100)
+            remote_agent_info.append(
+                {'name': card.name, 'description': card.description}
+            )
+        return remote_agent_info
+
+
+    async def send_message(
+            self, agent_name: str, task: str, tool_context: ToolContext
+    ):
+        """Sends a task to remote agent.
+        This will send a message to the remote agent named agent_name
+
+        Args:
+            agent_name: The name of the agent to send the task to
+            task: The comprehensive conversation context summary and goal to be achieved
+            tool_context: The tool context this method runs it
+
+        Yields:
+            A dictionary of JSON data
+        """
+
+        if agent not in self.remote_agent_connections:
+            raise ValueError(f'Agent {agent_name} not found')
+
+        print(
+            'sending message to',
+            agent_name,
+        )
+        state = tool_context.state
+        state['active_agent'] = agent_name
+        client = self.remote_agent_connections[agent_name]
+
+        if not client:
+            raise ValueError(f'Client not available for {agent_name}')
+
+        if 'context_id' in state:
+            context_id = state['context_id']
+        else:
+            context_id = str(uuid.uuid4())
+
+        message_id = ''
+        metadata = {}
+        if 'input_message_metadata' in state:
+            metadata.update(**state['input_message_metadata'])
+            if 'message_id' in state['input_message_metadata']:
+                message_id = state['input_message_metadata']['message_id']
+        if not message_id:
+            message_id = str(uuid.uuid4())
+
+        payload = {
+            'message': {
+                'role': 'user',
+                'parts': [
+                    {'type': 'text', 'text': task}
+                ],  # Use the 'task' argument here
+                'messageId': message_id,
+            },
+        }
+
+        if context_id:
+            payload['message']['contextId'] = context_id
+
+        message_request = SendMessageRequest(
+            id=message_id, params=MessageSendParams.model_validate(payload)
+        )
+
+        send_response: SendMessageResponse = await client.send_message(
+            message_request=message_request
+        )
+
+        print(
+            'send_response',
+            send_response.model_dump_json(exclude_none=True, indent=2),
+        )
+
+        if not isinstance(send_response.root, SendMessageSuccessResponse):
+            print('received non-success response. Aborting get task ')
+            return None
+
+        if not isinstance(send_response.root.result, Task):
+            print('received non-task response. Aborting get task ')
+            return None
+
+        return send_response.root.result
+
+
+def _get_initialized_coordinator_agent_sync() -> Agent:
+
+    async def _async_main() -> Agent:
+
+        coordinator_agent_instance = await CoordinatorAgent.create(
+            remote_agent_adresses=[
+                os.getenv('PRODUCT_INFORMATION_AGENT', 'http://localhost:10001')
+            ]
+        )
+        return coordinator_agent_instance.create_agent()
+
+    try:
+        return asyncio.run(_async_main())
+    except RuntimeError as e:
+        if 'asyncio.run cannot be called from running event loop' in str(e):
+            print(
+                f'Warning: Could not initialize Coordinator Agent with asycio.run(): {e}. '
+                'This can happen if an event loop is already running (e.g. in Jypyter)'
+                'Consider initializing Coordinator Agent within an async function in your ap'
+            )
+        raise
+
+
+root_agent = _get_initialized_coordinator_agent_sync()
